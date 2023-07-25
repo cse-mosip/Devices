1f4176e8888a41b92d4aa20d9d21bd666f28e026
Oshanath Rajawasam <oshanath.19@cse.mrt.ac.lk>
2023-07-25 17:17:11 +0530

    Able to directly decode and extract from bioValue

--- helpers/util.js
+++ helpers/util.js
@@ -1,18 +1,14 @@
 var fs = require('fs')
-const spawn = require("child_process").spawn;
+const execSync = require("child_process").execSync;
 
 // Returns a Buffer object with the raw bytes of a jpg image of a fingerprint.
-const extractImage = () => {
-    binary = fs.readFileSync('./helpers/data.bin', null)
+// run pip install pillow beforehand
+const extractImage = (bioValue) => {
+    binary = Buffer.from(bioValue, "base64")
     jp2ImageData = binary.slice(binary.indexOf(Buffer.from([0x00,0x00,0x00,0x0C,0x6A,0x50,0x20,0x20,0x0D,0x0A,0x87,0x0A])))
     fs.writeFileSync("./helpers/finger.jp2", jp2ImageData)
-    
-    const pythonProcess = spawn('python',["./helpers/imageConverter.py"]);
-    pythonProcess.stdout.on('data', (data) => {
-        console.log("Received from python")
-        console.log(data.toString())
-    });
-
+    const result = execSync("python helpers/imageConverter.py");
+    console.log(result.toString("utf8"));
     jpg = fs.readFileSync('./helpers/finger.jpg', null)
 
     fs.unlinkSync("./helpers/finger.jp2")
