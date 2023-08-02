c0422ba33400d0787b54cad8027dee4d9eab2e49
Oshanath Rajawasam <oshanath.19@cse.mrt.ac.lk>
2023-08-02 13:52:34 +0530

    keep jpg file in extract image

--- helpers/util.js
+++ helpers/util.js
@@ -3,18 +3,14 @@ const execSync = require("child_process").execSync;
 
 // Returns a Buffer object with the raw bytes of a jpg image of a fingerprint.
 // run pip install pillow beforehand
-const extractImage = (bioValue) => {
+const extractImage = (bioValue, name) => {
     binary = Buffer.from(bioValue, "base64")
     jp2ImageData = binary.slice(binary.indexOf(Buffer.from([0x00,0x00,0x00,0x0C,0x6A,0x50,0x20,0x20,0x0D,0x0A,0x87,0x0A])))
-    fs.writeFileSync("./helpers/finger.jp2", jp2ImageData)
+    fs.writeFileSync(`./helpers/finger.jp2`, jp2ImageData)
     const result = execSync("python helpers/imageConverter.py");
     console.log(result.toString("utf8"));
-    jpg = fs.readFileSync('./helpers/finger.jpg', null)
-
-    fs.unlinkSync("./helpers/finger.jp2")
-    fs.unlinkSync("./helpers/finger.jpg")
-
-    return jpg
+    fs.rename('./helpers/finger.jpg', `./helpers/${name}.jpg`)
+    fs.unlinkSync(`./helpers/finger.jp2`)
 }
 
 module.exports = {extractImage};
