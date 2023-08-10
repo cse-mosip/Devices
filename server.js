31d6d5998b6b70e6cfe84de837d2de7bedb5285c
Oshanath Rajawasam <oshanath.19@cse.mrt.ac.lk>
2023-08-10 17:58:58 +0530

    changed port to 1729

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const regController = require('./controllers/reg.controller');
 const { Server } = require("socket.io");
 const http = require('http');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 1729;
 
 //swagger API testing
 const swaggerUI = require('swagger-ui-express');
